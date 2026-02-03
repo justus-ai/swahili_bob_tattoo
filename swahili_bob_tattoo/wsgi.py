526149c94fa8fc78a3f70eb903d0b04f99726026
Justus marwa <83753891+justus-ai@users.noreply.github.com>
2026-02-03 10:13:55 +0000

    removing v1 in project name

--- swahili_bob_tattoo/wsgi.py
+++ swahili_bob_tattoo/wsgi.py
@@ -10,5 +10,5 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swahili_bob_tattoo_v1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swahili_bob_tattoo.settings')
 application = get_wsgi_application()
